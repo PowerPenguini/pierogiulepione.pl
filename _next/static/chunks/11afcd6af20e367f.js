e61e26cc2abd2f283d3ddf3964ced212249fbc4f
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2025-09-10 00:08:22 +0200

    Updates

--- _next/static/chunks/11afcd6af20e367f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/148a99eec4f094ca.js",
-  "static/chunks/turbopack-51d8747b14ef0413.js"
-])
--- _next/static/chunks/f9834b1a2998aa92.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/148a99eec4f094ca.js",
-  "static/chunks/turbopack-e65552bea82633f3.js"
-])
