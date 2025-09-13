bf04ca00b0bc1c92e8c6d0592e6b63aea27551fa
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2025-09-13 03:01:28 +0200

    Minor improvements

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? `/ulepionepierogi.pl` : "",
-  assetPrefix: isProd ? `/ulepionepierogi.pl/` : "",
   images: {
     unoptimized: true,
   },
